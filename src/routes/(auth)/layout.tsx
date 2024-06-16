bd27ae0ad117ffa09279a06f6e963409be78b8a2
harshmangalam <harshdev8218@gmail.com>
2024-06-16 13:28:06 +0530

    added layout for auth

--- src/routes/(auth)/layout.tsx
+++ src/routes/(auth)/layout.tsx
@@ -1,9 +1,13 @@
 import { Slot, component$ } from "@builder.io/qwik";
+import { Footer } from "~/components/footer";
 
-export default component$(()=>{
-    return (
-        <div>
-            <Slot />
-        </div>
-    )
-})
+export default component$(() => {
+  return (
+    <div class="flex h-full min-h-screen flex-col justify-between bg-muted">
+      <div class="grid flex-1 place-items-center">
+        <Slot />
+      </div>
+      <Footer />
+    </div>
+  );
+});
